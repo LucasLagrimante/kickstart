273298f149297c543779a6a513b4df3d11615a55
LucasL <lucaslagrimante@live.com>
2019-04-22 12:00:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-const { createServer } = require('http');
-const next = require('next');
+// server.js
+const next = require('next')
+const routes = require('./routes')
+const app = next({dev: process.env.NODE_ENV !== 'production'})
+const handler = routes.getRequestHandler(app)
 
-const app = next({
-  dev: process.env.NODE_ENV !== 'production',
-  conf: {
-    webpack: config => {
-      config.devtool = false;
-
-      for (const r of config.module.rules) {
-        if (r.loader === 'babel-loader') {
-          r.options.sourceMaps = false;
-        }
-      }
-
-      return config;
-    }
-  }
-});
-
-const routes = require('./routes');
-const handler = routes.getRequestHandler(app);
-
+// With express
+const express = require('express')
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
-    if (err) throw err;
-    console.log('Ready on localhost:3000');
-  });
-});
+  express().use(handler).listen(3000)
+})
+
+// Without express
+const {createServer} = require('http')
+app.prepare().then(() => {
+  createServer(handler).listen(3000)
+})
